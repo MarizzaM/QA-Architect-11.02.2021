80b304b3cab06a22a6a1ec10d4b1aa05732aaba2
MarizzaM <75089445+MarizzaM@users.noreply.github.com>
2021-02-11 20:21:20 +0200

    Update Celebrity.java

--- Hotel/Celebrity.java
+++ Hotel/Celebrity.java
@@ -6,7 +6,4 @@ import lombok.Setter;
 public class Celebrity extends VIP {
     private @Getter @Setter int countOfBed;
 
-    public void goParty(){
-        System.out.println("Mesiba-BA-BA-BA");
-    }
 }
